364622558acf3538140cb9c0398562eb2467d858
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-03-08 16:23:53 +0500

    pathmodule

---
+++ pathmodule.js
@@ -0,0 +1,7 @@
+const path = require('path');
+console.log(path.win32);
+console.log(path.basename('C:\\Users\\HHC\\Desktop\\nodejs'));
+console.log(path.dirname('C:\\Users\\HHC\\Desktop\\nodejs'));
+console.log(path.extname('C:\\Users\\HHC\\Desktop\\nodejs\\second.js'));
+console.log(path.extname(__filename));
+console.log(__filename);
