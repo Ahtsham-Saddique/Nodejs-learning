e4b3ae30f3fbd4bb4007699f18784707b0086a30
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-04-03 13:10:13 +0500

    CJS + callback

---
+++ filesystem.js
@@ -0,0 +1,18 @@
+// CJS+CALLBACK\\
+const fs = require('node:fs');
+
+
+fs.readFile('./a.txt','UTF-8',(err,data)=>
+            {
+                if(err)
+                {
+                    console.log("Error :",err);
+                    return
+                }
+                console.log(("File Content: ",data))
+
+            }
+            
+            );
+
+        console.log("Program continues ............");
