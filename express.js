7fbe5bd419196079009d2bfc61ea7c810bb55a93
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-04-11 16:45:07 +0500

    middleware concepts

--- express.js
+++ express.js
@@ -1,17 +1,36 @@
 import express from 'express'
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.use(function(req,res,next)
+{
+  console.log("Request received");
+ next();
+})
+app.use(function(req,res,next)
+{
+  console.log("Request received confirmation");
+ next();
 })
 
-app.get("/sham",(req,res)=>
+app.get('/', (req, res,next) => {
+  res.send('Hello World')
+  next();
+})
+
+app.get("/sham",(req,res,next)=>
 {
   res.send("THis is the sham tech '/n' howw e can help you please");
-  res.send(req);
+  
+  next();
 })
 ;
+
+app.use(function(req,res,next)
+{
+  console.log("Response received!!!!!!!!!!");
+ 
+})
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
