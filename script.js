3e821fd1aaa52b2686f0c4cbecb831238a18ed24
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-04-11 13:11:06 +0500

    express.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// CJS+CALLBACK\\
-const fs = require('node:fs');
+// // CJS+CALLBACK\\
+// const fs = require('node:fs');
 
 
-fs.readFile('./a.txt','UTF-8',(err,data)=>
-            {
-                if(err)
-                {
-                    console.log("Error :",err);
-                    return
-                }
-                console.log(("File Content: ",data))
+// fs.readFile('./a.txt','UTF-8',(err,data)=>
+//             {
+//                 if(err)
+//                 {
+//                     console.log("Error :",err);
+//                     return
+//                 }
+//                 console.log(("File Content: ",data))
 
-            }
+//             }
             
-            );
+//             );
 
         console.log("Program continues ............");
