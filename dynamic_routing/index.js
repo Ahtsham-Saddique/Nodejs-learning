df5aa633e44ad11491a86699f0cd22b5ef8e9a04
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-04-13 13:05:06 +0500

    ejs setup

--- dynamic_routing/index.js
+++ dynamic_routing/index.js
@@ -10,9 +10,15 @@ app.use(express.json());
 // parse URL-encoded form data
 app.use(express.urlencoded({extended:true}));
 
+
+// setup EJS as view engine
+app.set('view engine', 'ejs'); 
+
+
 app.get("/",function(req,res,next)
 {
-    res.send("Its working bro your doing a great job ,stay consistent");
+    // res.send("Its working bro your doing a great job ,stay consistent");
+    res.render("home");
 });
 app.listen(3000,()=>{
     console.log("Server is running on the link http://localhost:3000");
