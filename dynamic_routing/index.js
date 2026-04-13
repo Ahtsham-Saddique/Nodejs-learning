bb7ac9c99bf0e6c9f4c850821434eabef03abcc4
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-04-13 11:07:59 +0500

    basic settings & parsers for form

--- dynamic_routing/index.js
+++ dynamic_routing/index.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+import express from "express";
+
+const app = express();
+
+
+// take JSON request body → convert to JavaScript object
+app.use(express.json());
+
+
+// parse URL-encoded form data
+app.use(express.urlencoded({extended:true}));
+
+app.get("/",function(req,res,next)
+{
+    res.send("Its working bro your doing a great job ,stay consistent");
+});
+app.listen(3000,()=>{
+    console.log("Server is running on the link http://localhost:3000");
+})
